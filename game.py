5f93a4e6a50b2eaa61baa56b1ed46c3e80b8d89f
alonoh <alon444555@gmail.com>
2023-12-09 19:29:42 +0200

    first c

---
+++ game.py
@@ -0,0 +1,25 @@
+import random
+
+
+class Game:
+
+    def __init__(self):
+        self.cards = self.initialize_deck()
+
+    @staticmethod
+    def initialize_deck():
+        start_deck = [0, 0, 0]
+        for number in range(1, 11):
+            if number != 10:
+                start_deck += [number] * 5
+            elif number == 10:
+                start_deck += [number] * 8
+        return start_deck
+
+
+# Example usage:
+game = Game()
+
+
+print(game.cards)
+
