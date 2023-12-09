03a0c640a0bd2a7f6c75849fc8dbac05c1a021ee
alonoh <alon444555@gmail.com>
2023-12-09 21:48:02 +0200

    change

--- game.py
+++ game.py
@@ -3,9 +3,11 @@ import random
 
 class Game:
 
+    IMPOSSIBLE_LUCKY_CARDS = [0, 6]
+
     def __init__(self):
         self.cards = self.initialize_deck()
-        self.lucky_card = self.get_card()
+        self.lucky_card = self.get_card(zero_possible=False)
         self.cards_thrown = []
 
     @staticmethod
@@ -19,12 +21,21 @@ class Game:
         random.shuffle(start_deck)
         return start_deck
 
-    def get_card(self, take_from_stack=True):
+    def get_card(self, take_from_stack=True, zero_possible=True):
         if not take_from_stack:
             last_thrown = self.cards_thrown[-1]
             self.cards_thrown[:] = self.cards[:-1]
             return last_thrown
         else:
+            if not zero_possible:
+                while True:
+                    card = self.cards[-1]
+                    if card not in self.IMPOSSIBLE_LUCKY_CARDS:
+                        self.cards[:] = self.cards[:-1]
+                        return card
+                    else:
+                        random.shuffle(self.cards)
+
             card = self.cards[-1]
             self.cards[:] = self.cards[:-1]
             return card
