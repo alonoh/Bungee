fe04286354cf891c98e895f868a1bea1f022422d
alonoh <alon444555@gmail.com>
2023-12-09 22:12:31 +0200

    change

--- game.py
+++ game.py
@@ -2,7 +2,6 @@ import random
 
 
 class Game:
-
     IMPOSSIBLE_LUCKY_CARDS = [0, 6]
 
     def __init__(self):
@@ -21,24 +20,28 @@ class Game:
         random.shuffle(start_deck)
         return start_deck
 
-    def get_card(self, take_from_stack=True, zero_possible=True):
-        if not take_from_stack:
-            last_thrown = self.cards_thrown[-1]
-            self.cards_thrown[:] = self.cards[:-1]
-            return last_thrown
-        else:
-            if not zero_possible:
-                while True:
-                    card = self.cards[-1]
-                    if card not in self.IMPOSSIBLE_LUCKY_CARDS:
-                        self.cards[:] = self.cards[:-1]
-                        return card
-                    else:
-                        random.shuffle(self.cards)
+    def get_from_stack(self):
+        card = self.cards[-1]
+        self.cards[:] = self.cards[:-1]
+        return card
 
+    def get_from_thrown(self):
+        if len(self.cards_thrown) == 0:
+            return self.get_from_stack()
+
+        else:
+            last_thrown = self.cards_thrown[-1]
+            self.cards_thrown[:] = self.cards_thrown[:-1]
+            return last_thrown
+
+    def valid_lucky_card(self):
+        while True:
             card = self.cards[-1]
-            self.cards[:] = self.cards[:-1]
-            return card
+            if card not in self.IMPOSSIBLE_LUCKY_CARDS:
+                self.cards[:] = self.cards[:-1]
+                return card
+            else:
+                random.shuffle(self.cards)
 
     def throw_card(self, card):
         self.cards_thrown.append(card)
