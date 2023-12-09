2c822e7d9c8fb511c8da4fcbc6a48e6c5a077143
alonoh <alon444555@gmail.com>
2023-12-09 20:45:56 +0200

    changes

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ class Game:
 
     def __init__(self):
         self.cards = self.initialize_deck()
+        self.lucky_card = self.get_card()
 
     @staticmethod
     def initialize_deck():
@@ -14,10 +15,21 @@ class Game:
                 start_deck += [number] * 5
             elif number == 10:
                 start_deck += [number] * 8
+        random.shuffle(start_deck)
         return start_deck
 
+    def get_card(self):
+        card = self.cards[-1]
+        self.cards[:] = self.cards[0:-1]
+        return card
+
+    def thrown_card(self, card):
+        cards_thrown = []
+        new_cards_thrown = cards_thrown + card
 
 # Example usage:
 game = Game()
 
-print(game.cards)
+# print(game.cards)
+# print(game.get_card())
+# print(game.cards)
